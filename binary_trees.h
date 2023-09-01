50428a0d439b6d691b0ebaa6e4af394e7051967c
aycrown1 <ayoadeadedamola198@gmail.>
2023-09-01 10:15:20 +0100

    binary trees

--- binary_trees.h
+++ binary_trees.h
@@ -1,5 +1,5 @@
-#ifndef BINARY_TREES_H
-#define BINARY_TREES_H
+#ifndef _BINARY_TREES_H_
+#define _BINARY_TREES_H_
 #include <stdlib.h>
 #include <stdio.h>
 #include <stddef.h>
@@ -47,7 +47,7 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 
 
-binart_tree_t
+binary_tree_t
 *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 int binary_tree_is_complete(const binary_tree_t *tree);
